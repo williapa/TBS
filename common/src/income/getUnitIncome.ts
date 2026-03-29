39cdfe7f9239f64267114bf6212a83ae9efaf2a8
Paul Williams <williams.4049@osu.edu>
2026-03-28 22:04:56 -0700

    add income values for additional building types

--- common/src/income/getUnitIncome.ts
+++ common/src/income/getUnitIncome.ts
@@ -1,8 +1,27 @@
-import { MapItem } from "../types";
+import { MapItem, buildingUnitOptions} from "../types";
+
+const buildingIncomeMap = {
+  airport: 100,
+  bank: 1000,
+  capital: 200,
+  church: 0,
+  college: 0,
+  factory: 200,
+  house: 100,
+  lab: 300,
+  office: 400,
+  port: 200,
+  zoo: 100
+};
 
 const getUnitIncome = (item: MapItem): number => {
-  if (item.unit === "capital") return 100;
-  return 0;
+  
+  const unitType = item.unit;
+
+  if (buildingUnitOptions.indexOf(unitType) > 0) return 0;
+
+  return buildingIncomeMap[unitType];
+
 };
 
 export default getUnitIncome;
