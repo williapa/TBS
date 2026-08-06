0b2377c5a67a753da5e8745c24bd36f67d38b9c5
williapa <williams.4049@osu.edu>
2026-08-06 11:56:27 -0700

    test utils

--- server/src/data/createTable.ts
+++ server/src/data/createTable.ts
@@ -1,9 +1,13 @@
 import {
   CreateTableCommand,
   DescribeTableCommand,
+  waitUntilTableExists,
 } from '@aws-sdk/client-dynamodb';
+import { PutCommand } from '@aws-sdk/lib-dynamodb';
 import { client } from "./dynamoClient.js"
+import ddbDocClient from './docClient.js';
 import TableName from './tableName.js';
+import { TEST_USERS } from './testResources.js';
 
 async function tableExists(tableName: string) {
   try {
@@ -17,7 +21,7 @@ async function tableExists(tableName: string) {
   }
 }
 
-async function createTable() {
+async function createTableIfMissing() {
   const exists = await tableExists(TableName);
 
   if (exists) {
@@ -76,9 +80,37 @@ async function createTable() {
   const result = await client.send(command);
   console.log(`Created table "${TableName}".`);
   console.log(JSON.stringify(result, null, 2));
+
+  await waitUntilTableExists(
+    { client, maxWaitTime: 30 },
+    { TableName },
+  );
 }
 
-createTable().catch((err) => {
+async function createDummyUsers() {
+  for (const user of TEST_USERS) {
+    await ddbDocClient.send(
+      new PutCommand({
+        TableName,
+        Item: {
+          id: `user#${user.email}`,
+          sk: `meta#${user.email}`,
+          pin: user.pin,
+          wins: 0,
+          losses: 0,
+        },
+      }),
+    );
+    console.log(`Created or updated dummy user "${user.email}".`);
+  }
+}
+
+async function main() {
+  await createTableIfMissing();
+  await createDummyUsers();
+}
+
+main().catch((err) => {
   console.error('Failed to create table:', err);
   process.exit(1);
 });
--- server/src/data/dynamoClient.ts
+++ server/src/data/dynamoClient.ts
@@ -3,7 +3,7 @@ import { DynamoDBClient } from '@aws-sdk/client-dynamodb';
 // todo: obviously this is for local only, need some process.env.? variable for dev/prod (stage).
 // for prod, region & creds would go here
 export const client = new DynamoDBClient({
-  endpoint: 'http://host.docker.internal:8000',
+  endpoint: process.env.DYNAMODB_ENDPOINT ?? 'http://localhost:8000',
   region: 'us-east-1',
   credentials: {
     accessKeyId: 'dummy',
